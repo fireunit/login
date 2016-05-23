3de65a8b5e81a0c50b61354360714432bddfa564
doug <doug@getweave.com>
2016-05-23 11:39:25 -0600

    initial commit

---
+++ Podfile
@@ -0,0 +1,21 @@
+platform :ios, '9.0'
+
+target 'login' do
+    use_frameworks!
+
+  # Pods for login
+  pod 'Firebase'
+  pod 'Firebase/Auth'
+  pod 'Material'
+  
+  target 'loginTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'loginUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ login/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  login
+//
+//  Created by doug on 5/23/16.
+//  Copyright © 2016 fireunit. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        FIRApp.configure()
+        
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        window!.rootViewController = LoginViewController()
+        window!.makeKeyAndVisible()
+        
+        return true
+    }
+
+}
+
